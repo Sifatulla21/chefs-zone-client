04f789f834aacca55e4128f23c5c0ef02d15d59e
Sifatulla21 <sifat2109@gmail.com>
2023-05-03 12:19:24 +0600

    set private route

--- src/Routes/Route.jsx
+++ src/Routes/Route.jsx
@@ -5,6 +5,7 @@ import ChefCard from "../Pages/Home/ChefCard/ChefCard";
 import Home from "../Pages/Home/Home/Home";
 import Login from "../Pages/Login/Login/Login";
 import Register from "../Pages/Login/Register/Register";
+import PrivateRoute from "./PrivateRoute";
 
 const router = createBrowserRouter([
     {
@@ -17,7 +18,7 @@ const router = createBrowserRouter([
             },
             {
                 path:':id',
-                element:<ChefDetails></ChefDetails>,
+                element:<PrivateRoute><ChefDetails></ChefDetails></PrivateRoute> ,
                 loader: ({params}) => fetch(`http://localhost:5000/chef/${params.id}`)
             },
             {
