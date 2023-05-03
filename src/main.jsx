9f9cb70829dec8f438ca3ff8f8382f326feac81a
Sifatulla21 <sifat2109@gmail.com>
2023-05-03 11:29:04 +0600

    set auth provider and make env local

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,16 @@ import { RouterProvider } from 'react-router-dom'
 import router from './Routes/Route'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import AuthProvider from './Providers/AuthProvider'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router}></RouterProvider>
+
+    <AuthProvider>
+      <RouterProvider router={router}></RouterProvider>
+    </AuthProvider>
+
+
+
   </React.StrictMode>,
 )
